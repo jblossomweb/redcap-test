b62cb76ab75066536c60effee2b4429b9d7ee877
John Blossom <jblossom.web@gmail.com>
2016-04-15 22:46:46 -0400

    scaffold express/mysql

---
+++ app.js
@@ -0,0 +1,67 @@
+var express = require("express")
+var orm = require("orm")
+var fs = require('fs')
+var path = require("path")
+var logger = require('morgan')
+var bodyParser = require('body-parser')
+
+var config = require('./config')
+var app = express()
+
+// connect to database
+orm.db = orm.connect(config[config.db.type].connection, function(error, db){
+	if(!error) return
+	throw(error)
+})
+
+// view engine setup
+app.set('views', path.join(__dirname, 'templates'))
+app.set('view engine', 'pug') // the artist formerly known as jade
+
+// static server setup 
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/libs', express.static('bower_components'))
+
+// parser setup
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// logger setup
+if (config.node.env === 'development') {
+  app.use(logger('dev'))
+}
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (config.node.env === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500)
+    res.json(err)
+  })
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
+  res.json({
+    message: err.message,
+    name: err.name,
+    errors: err.errors || null
+  })
+})
+
+app.listen(config.express.port, function () {
+  console.log('listening on port '+config.express.port)
+})
+
+module.exports = app
---
+++ config.js
@@ -0,0 +1,24 @@
+var fs = require('fs')
+var env = require('node-env-file')
+
+if (fs.existsSync('.env')) {
+  env('.env')
+}
+
+module.exports = {
+	site: {
+		title: process.env.SITE_TITLE || "Redcap Test",
+	},
+	node: {
+		env: process.env.NODE_ENV || 'development'
+	},
+	express: {
+		port: process.env.EXPRESS_PORT || 3000
+	},
+	db: {
+		type: 'mysql'
+	},
+    mysql: {
+        connection: process.env.MYSQL_CONNECTION
+    }
+}
